5747540c6090508b00b4f51a01f5e0918a11f02f
Yuri Nakashima <yuri310@gmail.com>
2017-01-24 15:04:13 -0800

    rack cors modified in application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,12 @@ module ShopRails2
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('lib')
     config.assets.initialize_on_precompile = false
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
