7b933a417e7c2edf81cdcf95f5008eb1ed4fea68
Yuri Nakashima <yuri310@gmail.com>
2017-01-24 19:16:34 -0800

    updated cors rack

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, "Rack:: Cors" do
   allow do
-    origins 'http://fashionhound.com.s3-website-us-west-2.amazonaws.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
